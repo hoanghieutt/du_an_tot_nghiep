341a10b7caf05256cbee77fd8193ded2937e24c3
hoanghieutt <hhieu5764@gmail.com>
2024-01-15 23:57:04 +0700

    proj

---
+++ duantotnghiep/src/test/java/com/poly/sd18/duantotnghiep/DuantotnghiepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.sd18.duantotnghiep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuantotnghiepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
